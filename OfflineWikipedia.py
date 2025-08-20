a3c272af6b6cb6c01a3b9d015f0ecfc7a6801bad
Harlow <secrethallow@outlook.com>
2025-08-20 00:56:33 -0500

    It's no longer barebones :3
    I struggled with this one but it now has more options(even if there is only two useful ones)
    
    Now is a functional loop

--- OfflineWikipedia.py
+++ OfflineWikipedia.py
@@ -1,10 +1,48 @@
 import wikipedia
 import sys
+import os
+import time
 
-while True:
-    try:
-        userinput = input("> ")
-        result = wikipedia.summary(userinput)
-        print(result)
-    except:
-        print("Something went wrong please try again!")
+def choosey():
+    while True:
+        print("Welcome to OfflineWikipedia")
+        print("Press 1 for Summary \nPress 2 for Pages(It is useless tho) \nPress 3 for Search \nor type 'q' to quit it\n")
+        choose_it = input("> ")
+        if choose_it == "1":
+            os.system('cls')
+            while True:
+                try:
+                    userinput = input("> ")
+                    result = wikipedia.summary(userinput)
+                    print(result)
+                except Exception as e:
+                    print("Something went wrong please try again!", e)
+        elif choose_it == "2":
+            os.system('cls')
+            while True:
+                try:
+                    userinput = input("> ")
+                    result = wikipedia.page(userinput)
+                    print(result)
+                except Exception as e:
+                    print("Something went wrong please try again!", e)
+        elif choose_it == "3":
+            os.system('cls')
+            while True:
+                try:
+                    userinput = input("> ")
+                    result = wikipedia.search(userinput)
+                    print(result)
+                except Exception as e:
+                    print("Something went wrong please try again!", e)
+        elif choose_it.lower() == "q":
+            "Goodbye"
+            break
+        else:
+            os.system('cls')
+            print("Not a valid input there, champ")
+            time.sleep(1)
+            os.system('cls')
+            continue
+
+choosey()
